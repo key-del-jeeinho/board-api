7c9c1f3902f1827b1b89ff74019ddf4bf61ae47f
raul <develop.raul@gmail.com>
2022-08-23 15:35:54 +0900

    Initial commit

---
+++ src/main/kotlin/gg/dak/board_api/BoardApiApplication.kt
@@ -0,0 +1,11 @@
+package gg.dak.board_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoardApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoardApiApplication>(*args)
+}
---
+++ src/test/kotlin/gg/dak/board_api/BoardApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package gg.dak.board_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoardApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
