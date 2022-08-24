f5eac3a367e930d1f79bc02929349f3371ffcbd4
raul <develop.raul@gmail.com>
2022-08-24 14:48:23 +0900

    [FIX] Account 엔티티 ID생성전략 명시

--- src/main/kotlin/gg/dak/board_api/domain/account/data/enitty/Account.kt
+++ src/main/kotlin/gg/dak/board_api/domain/account/data/enitty/Account.kt
@@ -1,11 +1,14 @@
 package gg.dak.board_api.domain.account.data.enitty
 
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
 class Account (
-    @Id val idx: Long,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val idx: Long,
     val nickName: String,
     val id: String,
     val encodedPassword: String
