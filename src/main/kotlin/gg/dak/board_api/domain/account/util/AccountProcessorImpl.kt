0e88442aa9247bbbb52df1cc02421eceea8f743d
raul <develop.raul@gmail.com>
2022-08-24 13:21:05 +0900

    [ADD] 테스트를 통과하도록 AccountProcessorImpl 구현

--- src/main/kotlin/gg/dak/board_api/domain/account/util/AccountProcessorImpl.kt
+++ src/main/kotlin/gg/dak/board_api/domain/account/util/AccountProcessorImpl.kt
@@ -7,7 +7,15 @@ import org.springframework.security.crypto.password.PasswordEncoder
 class AccountProcessorImpl(
     private val passwordEncoder: PasswordEncoder
 ): AccountProcessor {
-    override fun process(operation: OperationType, dto: AccountDto): AccountDto {
-        TODO()
-    }
+    override fun process(operation: OperationType, dto: AccountDto): AccountDto =
+        when(operation) {
+            OperationType.REGISTER -> processRegister(dto)
+        }
+
+    private fun processRegister(dto: AccountDto): AccountDto =
+        encodePassword(dto).copy(idx = 0)
+
+    private fun encodePassword(dto: AccountDto): AccountDto =
+        if(dto.isPasswordEncoded) dto
+        else dto.copy(password = passwordEncoder.encode(dto.password), isPasswordEncoded = true)
 }
