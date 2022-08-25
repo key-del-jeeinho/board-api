2c02383576978919c6d397dcd7ac24e947b60423
raul <develop.raul@gmail.com>
2022-08-25 09:12:17 +0900

    [ADD] RedisUuidTokenGenerator 구현체 작성

---
+++ src/main/kotlin/gg/dak/board_api/domain/account/util/RedisUuidTokenGenerator.kt
@@ -0,0 +1,17 @@
+package gg.dak.board_api.domain.account.util
+
+import gg.dak.board_api.domain.account.data.enitty.UuidToken
+import gg.dak.board_api.domain.account.repository.UuidTokenRepository
+import org.springframework.stereotype.Component
+import java.util.UUID
+
+@Component
+class RedisUuidTokenGenerator(
+    private val uuidTokenRepository: UuidTokenRepository
+): UuidTokenGenerator {
+    override fun generate(payload: Map<String, String>, expireSecond: Long): String =
+        UUID.randomUUID().toString()
+            .let { UuidToken(it, payload, expireSecond) }
+            .also { uuidTokenRepository.save(it) }
+            .token
+}
