cb2387730dc8e587f329588893b02d27b3f372ea
raul <develop.raul@gmail.com>
2022-08-26 23:03:11 +0900

    [FIX] PostConverterImpl에 추가된 스팩 구현

--- src/main/kotlin/gg/dak/board_api/domain/post/util/PostConverterImpl.kt
+++ src/main/kotlin/gg/dak/board_api/domain/post/util/PostConverterImpl.kt
@@ -1,9 +1,12 @@
 package gg.dak.board_api.domain.post.util
 
 import gg.dak.board_api.domain.post.data.dto.PostDto
+import gg.dak.board_api.domain.post.data.entity.Post
 import gg.dak.board_api.domain.post.data.request.CreatePostRequest
 import gg.dak.board_api.domain.post.data.response.CreatePostResponse
+import org.springframework.stereotype.Component
 
+@Component
 class PostConverterImpl: PostConverter {
     override fun toDto(request: CreatePostRequest, writerIdx: Long): PostDto =
         PostDto(
@@ -13,7 +16,19 @@ class PostConverterImpl: PostConverter {
             writerId = writerIdx
         )
 
-    override fun toResponse(dto: PostDto): CreatePostResponse {
-        TODO("Not yet implemented")
-    }
+    override fun toDto(entity: Post): PostDto = PostDto(
+        idx = entity.idx,
+        writerId = entity.writerIdx,
+        title = entity.title,
+        content = entity.content
+    )
+
+    override fun toResponse(dto: PostDto): CreatePostResponse = CreatePostResponse(idx = dto.idx)
+
+    override fun toEntity(dto: PostDto): Post = Post(
+        idx = dto.idx,
+        writerIdx = dto.writerId,
+        title = dto.title,
+        content = dto.content
+    )
 }
