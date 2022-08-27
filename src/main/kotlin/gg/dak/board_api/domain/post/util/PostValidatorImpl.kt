5e20c450e7e1fd7e7b4d5916342c343e4b631102
raul <develop.raul@gmail.com>
2022-08-27 14:35:22 +0900

    [ADD] PostValidatorTest 테스트 작성 및 테스트를 통과하는 비즈니스로직 구현
    - PostValidatorImpl 구현체 작성

---
+++ src/main/kotlin/gg/dak/board_api/domain/post/util/PostValidatorImpl.kt
@@ -0,0 +1,23 @@
+package gg.dak.board_api.domain.post.util
+
+import gg.dak.board_api.domain.post.config.PostProperties
+import gg.dak.board_api.domain.post.data.dto.PostDto
+import gg.dak.board_api.domain.post.data.type.PostOperationType
+import gg.dak.board_api.domain.post.repository.DailyPostCountRepository
+import gg.dak.board_api.global.common.exception.PolicyValidationException
+
+class PostValidatorImpl(
+    private val postProperties: PostProperties,
+    private val dailyPostCountRepository: DailyPostCountRepository
+): PostValidator {
+    override fun validate(type: PostOperationType, dto: PostDto) {
+        when(type) {
+            PostOperationType.CREATE -> validateDailyPostLimit(dto)
+        }
+    }
+
+    private fun validateDailyPostLimit(dto: PostDto) =
+        dailyPostCountRepository.findByAccountIdx(dto.writerIdx)
+            .let { if(it.isEmpty) return else it.get()} //만약, 일일작성횟수 정보가 없다면, 검증중인 게시글을 오늘 첫 작성 게시글로 취급한다.
+            .let { if(it.count >= postProperties.dailyPostLimit) throw PolicyValidationException("포스트 작성 정책을 위반하였습니다!", "일일 작성한도를 초과하였습니다.") }
+}
---
+++ src/test/kotlin/gg/dak/board_api/domain/post/util/PostValidatorTest.kt
@@ -0,0 +1,51 @@
+package gg.dak.board_api.domain.post.util
+
+import gg.dak.board_api.domain.post.config.PostProperties
+import gg.dak.board_api.domain.post.data.dto.PostDto
+import gg.dak.board_api.domain.post.data.entity.DailyPostCount
+import gg.dak.board_api.domain.post.data.type.PostOperationType
+import gg.dak.board_api.domain.post.repository.DailyPostCountRepository
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import java.util.*
+import kotlin.random.Random
+
+class PostValidatorTest {
+    private lateinit var postProperties: PostProperties
+    private lateinit var dailyPostCountRepository: DailyPostCountRepository
+    private lateinit var target: PostValidator
+
+    @BeforeEach
+    fun setUp() {
+        postProperties = mock()
+        dailyPostCountRepository = mock()
+        target = PostValidatorImpl(postProperties, dailyPostCountRepository)
+    }
+
+    /* PolicyValidator - 게시글 생성 로즉 검증 성공테스트
+    PolicyValidator.validate(PostOperationType.CREATE, ?: PostDto)
+    해당 게시판에 대해, 작성자가 일일 작성 가능 횟수를 초과했는지 검증한다.
+     */
+    @Test @DisplayName("PolicyValidator - 게시글 생성 로직 검증 성공테스트A")
+    fun testValidateCreatePost_positive() {
+        //given
+        val dailyPostLimit = (1..1000).random()
+        val count = (1..999).filter { dailyPostLimit > it }.random()
+        val accountIdx = Random.nextLong()
+        val dto = mock<PostDto>()
+        val countEntity = mock<DailyPostCount>()
+        val optional = Optional.of(countEntity)
+
+        //when
+        whenever(postProperties.dailyPostLimit).thenReturn(dailyPostLimit)
+        whenever(dailyPostCountRepository.findByAccountIdx(accountIdx)).thenReturn(optional)
+        whenever(countEntity.count).thenReturn(count)
+
+        //then
+        assertDoesNotThrow { target.validate(PostOperationType.CREATE, dto) }
+    }
+}
