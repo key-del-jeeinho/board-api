98864266dd3a212d5c581b83c8fe2fc911b6e15f
raul <develop.raul@gmail.com>
2022-08-27 19:52:00 +0900

    [ADD] PostValidatorTest 테스트 작성 및 이를 통과하는 비즈니스로직 구현
    - PostValidatorImpl 추가스팩 구현

--- src/test/kotlin/gg/dak/board_api/domain/post/util/PostValidatorTest.kt
+++ src/test/kotlin/gg/dak/board_api/domain/post/util/PostValidatorTest.kt
@@ -3,9 +3,13 @@ package gg.dak.board_api.domain.post.util
 import gg.dak.board_api.domain.post.config.PostProperties
 import gg.dak.board_api.domain.post.data.dto.PostDto
 import gg.dak.board_api.domain.post.data.entity.DailyPostCount
+import gg.dak.board_api.domain.post.data.entity.Post
 import gg.dak.board_api.domain.post.data.type.BoardType
 import gg.dak.board_api.domain.post.data.type.PostOperationType
 import gg.dak.board_api.domain.post.repository.DailyPostCountRepository
+import gg.dak.board_api.domain.post.repository.PostRepository
+import gg.dak.board_api.global.account.data.dto.AccountDto
+import gg.dak.board_api.global.security.service.LoginAccountService
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -17,21 +21,25 @@ import kotlin.random.Random
 
 class PostValidatorTest {
     private lateinit var postProperties: PostProperties
+    private lateinit var postRepository: PostRepository
     private lateinit var dailyPostCountRepository: DailyPostCountRepository
+    private lateinit var loginAccountService: LoginAccountService
     private lateinit var target: PostValidator
 
     @BeforeEach
     fun setUp() {
         postProperties = mock()
+        postRepository = mock()
         dailyPostCountRepository = mock()
-        target = PostValidatorImpl(postProperties, dailyPostCountRepository)
+        loginAccountService = mock()
+        target = PostValidatorImpl(postProperties, postRepository, dailyPostCountRepository, loginAccountService)
     }
 
-    /* PolicyValidator - 게시글 생성 로즉 검증 성공테스트
+    /* PolicyValidator - 게시글 작성 로직 검증 성공테스트
     PolicyValidator.validate(PostOperationType.CREATE, ?: PostDto)
-    해당 게시판에 대해, 작성자가 일일 작성 가능 횟수를 초과했는지 검증한다.
+    작성자가 일일 작성 가능 횟수를 초과하였다면, 게시글을 작성할 수 없다.
      */
-    @Test @DisplayName("PolicyValidator - 게시글 생성 로직 검증 성공테스트A")
+    @Test @DisplayName("PolicyValidator - 게시글 작성 로직 검증 성공테스트A")
     fun testValidateCreatePost_positive() {
         //given
         val board = BoardType.values().random()
@@ -50,5 +58,30 @@ class PostValidatorTest {
 
         //then
         assertDoesNotThrow { target.validate(PostOperationType.CREATE, dto) }
+    }
+    //TODO 일일 작성 게시글 정보가 없을 경우(오늘 첫 작성일 경우) 성공테스트 작성
+
+    /* PolicyValidator - 게시글 삭제 로직 검증 성공테스트
+    PolicyValidator.validate(PostOperationType.DELETE, ?: PostDto)
+    요청자가 작성자가 아니라면, 게시글을 삭제할 수 없다.
+    TODO: 랭킹에 등록되어있는 게시글은 삭제할 수 없다.
+     */
+    @Test @DisplayName("PolicyValidator - 게시글 삭제 로직 검증 성공테스트")
+    fun testValidateDeletePost_positive() {
+        //given
+        val dto = mock<PostDto>()
+        val entity = mock<Post>()
+        val ownerIdx = Random.nextLong()
+        val optional = Optional.of(entity)
+        val accountDto = mock<AccountDto>()
+
+        //when
+        whenever(postRepository.findById(dto.idx)).thenReturn(optional)
+        whenever(entity.writerIdx).thenReturn(ownerIdx)
+        whenever(loginAccountService.getLoginAccount()).thenReturn(accountDto)
+        whenever(accountDto.idx).thenReturn(ownerIdx)
+
+        //then
+        assertDoesNotThrow { target.validate(PostOperationType.DELETE, dto) }
     }
 }
