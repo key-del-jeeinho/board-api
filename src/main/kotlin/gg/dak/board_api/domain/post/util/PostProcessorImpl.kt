db56449340f26c518a11bc164cbdcceab6273c32
raul <develop.raul@gmail.com>
2022-08-27 19:34:04 +0900

    [ADD] PostProcessorTest에 테스트 추가 및 이를 통과하는 비즈니스로직 구현
    - PostProcessorImpl 미구현된 분기 구현

--- src/main/kotlin/gg/dak/board_api/domain/post/util/PostProcessorImpl.kt
+++ src/main/kotlin/gg/dak/board_api/domain/post/util/PostProcessorImpl.kt
@@ -9,6 +9,6 @@ class PostProcessorImpl: PostProcessor {
     override fun process(operation: PostOperationType, dto: PostDto): PostDto =
         when(operation) {
             PostOperationType.CREATE -> dto.copy(idx = 0) //게시글 생성시, 인덱스를 0으로 초기화한다.
-            PostOperationType.DELETE -> TODO()
+            PostOperationType.DELETE -> dto //게시글 삭제시, 어떠한 처리 없이, 그대로 반환한다.
         }
 }
