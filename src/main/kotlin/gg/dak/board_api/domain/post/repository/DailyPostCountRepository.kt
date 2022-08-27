e07bf1b58bed238aaafa7cda9ec0d27416ec6e1d
raul <develop.raul@gmail.com>
2022-08-27 14:34:53 +0900

    [FIX] DailyPostCountRepository 잘못된 메서드 스팩 수정

--- src/main/kotlin/gg/dak/board_api/domain/post/repository/DailyPostCountRepository.kt
+++ src/main/kotlin/gg/dak/board_api/domain/post/repository/DailyPostCountRepository.kt
@@ -2,7 +2,8 @@ package gg.dak.board_api.domain.post.repository
 
 import gg.dak.board_api.domain.post.data.entity.DailyPostCount
 import org.springframework.data.repository.CrudRepository
+import java.util.*
 
 interface DailyPostCountRepository: CrudRepository<DailyPostCount, Long> {
-    fun findByAccountIdx(accountIdx: Long)
+    fun findByAccountIdx(accountIdx: Long): Optional<DailyPostCount>
 }
