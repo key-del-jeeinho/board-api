04d72945b46a5d0c678117a795ffd63b371976b7
raul <develop.raul@gmail.com>
2022-08-28 10:18:08 +0900

    [ADD] PostQueryConverterImpl 구현체 작성

---
+++ src/main/kotlin/gg/dak/board_api/domain/post/util/PostQueryConverterImpl.kt
@@ -0,0 +1,17 @@
+package gg.dak.board_api.domain.post.util
+
+import gg.dak.board_api.domain.post.data.dto.PostDto
+import gg.dak.board_api.domain.post.data.response.PostQueryResponse
+import org.springframework.stereotype.Component
+
+@Component
+class PostQueryConverterImpl: PostQueryConverter {
+    override fun toResponse(dto: PostDto): PostQueryResponse = PostQueryResponse(
+        idx = dto.idx,
+        writerIdx = dto.writerIdx,
+        title = dto.title,
+        content = dto.content,
+        category = dto.category,
+        board = dto.board,
+    )
+}
