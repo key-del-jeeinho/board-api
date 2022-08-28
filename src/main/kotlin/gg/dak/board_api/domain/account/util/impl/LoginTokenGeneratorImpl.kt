ca80a710d3441cbc40b87731ef71e5d7909e15d6
raul <develop.raul@gmail.com>
2022-08-28 16:20:52 +0900

    [FIX] TDD방식으로 로그인시 기존 재발급 토큰 삭제 기능 구현

--- src/main/kotlin/gg/dak/board_api/domain/account/util/impl/LoginTokenGeneratorImpl.kt
+++ src/main/kotlin/gg/dak/board_api/domain/account/util/impl/LoginTokenGeneratorImpl.kt
@@ -4,6 +4,8 @@ import gg.dak.board_api.domain.account.config.LoginProperties
 import gg.dak.board_api.domain.account.data.dto.LoginTokenDto
 import gg.dak.board_api.domain.account.data.event.LoginTokenCreateEvent
 import gg.dak.board_api.domain.account.data.type.TokenType
+import gg.dak.board_api.domain.account.repository.RefreshTokenRepository
+import gg.dak.board_api.domain.account.repository.UuidTokenRepository
 import gg.dak.board_api.domain.account.util.JwtTokenGenerator
 import gg.dak.board_api.domain.account.util.LoginTokenGenerator
 import gg.dak.board_api.global.account.util.UuidTokenGenerator
@@ -16,14 +18,18 @@ class LoginTokenGeneratorImpl(
     private val loginProperties: LoginProperties,
     private val jwtTokenGenerator: JwtTokenGenerator,
     @Qualifier("volatility") private val uuidTokenGenerator: UuidTokenGenerator,
-    private val applicationEventPublisher: ApplicationEventPublisher
+    private val applicationEventPublisher: ApplicationEventPublisher,
+    private val uuidTokenRepository: UuidTokenRepository,
+    private val refreshTokenRepository: RefreshTokenRepository
 ) : LoginTokenGenerator {
     override fun generate(id: String): LoginTokenDto =
-        jwtTokenGenerator.generate( //accessToken을 발급합니다.
+        refreshTokenRepository.findById(id) //기존에 재발급토큰이 존재하였는지 검사합니다.
+            .let { if(it.isPresent) uuidTokenRepository.deleteById(it.get().token) } //기존에 재발금토큰이 존재하였다면, 이를 제거합니다.
+            .let { jwtTokenGenerator.generate( //accessToken을 발급합니다.
                 mapOf(
                     "id" to id,
                     "type" to TokenType.LOGIN_ACCESS.key
-                ), loginProperties.accessTokenProperties.expireSecond)
+                ), loginProperties.accessTokenProperties.expireSecond) }
             .let { it to uuidTokenGenerator.generate( //refreshToken을 발급합니다.
                 mapOf(
                     "id" to id,
