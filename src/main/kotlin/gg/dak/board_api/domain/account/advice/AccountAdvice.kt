2da7c9bb20b1d7ad0dbe9dc049f99cf3aafedb0f
raul <develop.raul@gmail.com>
2022-08-28 17:24:13 +0900

    [ADD] Account 및 Post관련 Advice 작성

--- src/main/kotlin/gg/dak/board_api/domain/account/advice/AccountAdvice.kt
+++ src/main/kotlin/gg/dak/board_api/domain/account/advice/AccountAdvice.kt
@@ -2,6 +2,7 @@ package gg.dak.board_api.domain.account.advice
 
 import gg.dak.board_api.domain.account.controller.AccountController
 import gg.dak.board_api.domain.account.exception.UnknownUuidTokenException
+import gg.dak.board_api.global.account.exception.UnknownAccountException
 import gg.dak.board_api.global.error.ErrorResponse
 import gg.dak.board_api.global.error.ErrorStatus
 import org.springframework.http.ResponseEntity
@@ -9,11 +10,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice(basePackageClasses = [AccountController::class])
-class AccountControllerAdvice {
+class AccountAdvice {
     @ExceptionHandler(UnknownUuidTokenException::class)
     fun handle(e: UnknownUuidTokenException) =
         ErrorResponse(
             status = ErrorStatus.UNKNOWN_TOKEN,
+            message = e.getErrorMessage(),
+            details = e.getErrorDetails()
+        ).let { ResponseEntity.badRequest().body(it) }
+
+    @ExceptionHandler(UnknownAccountException::class)
+    fun handle(e: UnknownAccountException) =
+        ErrorResponse(
+            status = ErrorStatus.UNKNOWN_ACCOUNT,
             message = e.getErrorMessage(),
             details = e.getErrorDetails()
         ).let { ResponseEntity.badRequest().body(it) }
