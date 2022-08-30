c66448b70fb279b307cb3a11e0510e52d2b4916d
raul <develop.raul@gmail.com>
2022-08-30 15:13:25 +0900

    [FIX] API 설정 추가

--- src/main/kotlin/gg/dak/board_api/global/swagger/config/SwaggerConfig.kt
+++ src/main/kotlin/gg/dak/board_api/global/swagger/config/SwaggerConfig.kt
@@ -2,10 +2,12 @@ package gg.dak.board_api.global.swagger.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.ApiKey
 import springfox.documentation.service.AuthorizationScope
+import springfox.documentation.service.Contact
 import springfox.documentation.service.SecurityReference
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spi.service.contexts.SecurityContext
@@ -15,12 +17,13 @@ import springfox.documentation.spring.web.plugins.Docket
 class SwaggerConfig {
     @Bean
     fun docket(): Docket = Docket(DocumentationType.OAS_30)
-        .select()
-        .apis(RequestHandlerSelectors.basePackage("gg.dak.board_api"))
-        .paths(PathSelectors.any())
-        .build()
-        .securityContexts(listOf(securityContext()))
-        .securitySchemes(listOf(apiKey()))
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("gg.dak.board_api"))
+            .paths(PathSelectors.any())
+            .build()
+            .apiInfo(apiInfo())
+            .securityContexts(listOf(securityContext()))
+            .securitySchemes(listOf(apiKey()))
 
     @Bean
     fun securityContext(): SecurityContext =
@@ -34,4 +37,10 @@ class SwaggerConfig {
             .let { listOf(SecurityReference("Authorization", it)) }
 
     private fun apiKey() = ApiKey("Authorization", "Authorization", "header")
+
+    private fun apiInfo() = ApiInfoBuilder()
+            .title("Board API")
+            .description("소규모 게시글 서비스 API입니다. (Authorization 헤더 사용시 'Bearer 'prefix를 붙여주세요!)")
+            .contact(Contact("Raul", "https://www.github.com/key-del-jeeinho", "recruit.raul@gmail.com"))
+            .version("1.0").build()
 }
