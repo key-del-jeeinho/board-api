f4adfb409216833247848ef38a8c346d51562546
raul <develop.raul@gmail.com>
2022-08-30 12:54:07 +0900

    [ADD] 로그인에 대한 통합테스트 작성

--- src/test/kotlin/gg/dak/board_api/domain/post/AccountIntegrationTest.kt
+++ src/test/kotlin/gg/dak/board_api/domain/post/AccountIntegrationTest.kt
@@ -1,9 +1,11 @@
 package gg.dak.board_api.domain.post
 
 import gg.dak.board_api.IntegrationTestBase
+import gg.dak.board_api.domain.account.data.request.LoginRequest
 import gg.dak.board_api.domain.account.data.request.RegisterRequest
 import gg.dak.board_api.global.account.repository.AccountRepository
 import gg.dak.board_api.test_utils.TestComponentSource
+import gg.dak.board_api.test_utils.TestEnvironment.createAccount
 import gg.dak.board_api.test_utils.TestUtil
 import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.notNullValue
@@ -24,6 +26,7 @@ class AccountIntegrationTest: IntegrationTestBase() {
     fun setUp() {
         TestComponentSource.initializeMockMvc(mvc)
         TestComponentSource.initializeObjectMapper(objectMapper)
+        accountRepository.deleteAll()
     }
 
     @Test @DisplayName("회원가입 통합테스트 - 회원가입 성공")
@@ -34,7 +37,6 @@ class AccountIntegrationTest: IntegrationTestBase() {
 
         val request = RegisterRequest(nickname, id, password)
 
-        accountRepository.deleteAll()
 
         val resultAction = mvc.perform(post("/api/v1/account/register")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -45,5 +47,23 @@ class AccountIntegrationTest: IntegrationTestBase() {
         resultAction
                 .andExpect(status().isOk)
                 .andExpect(jsonPath("accountIdx", `is`(notNullValue())))
+    }
+
+    @Test @DisplayName("로그인 통합테스트 - 로그인 성공")
+    fun testLogin() {
+        val account = createAccount()
+
+        val request = LoginRequest(account.id, account.password)
+
+        val resultAction = mvc.perform(post("/api/v1/account/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo{ print(it.response.contentAsString) }
+
+        resultAction
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("accessToken", `is`(notNullValue())))
+                .andExpect(jsonPath("refreshToken", `is`(notNullValue())))
     }
 }
